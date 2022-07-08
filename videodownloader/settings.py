521c5eaeeeb5511d17a33f14996162f1c9b0c60c
MaazShahid6229 <me.maazshahid@gmail.com>
2022-07-08 12:24:02 +0500

    Setting update

--- videodownloader/settings.py
+++ videodownloader/settings.py
@@ -29,7 +29,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
